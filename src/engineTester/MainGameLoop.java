1d18b3a6db0a8d6434ca7752f2a7ff4eafbf37a5
KJ <kjbrackish@gmail.com>
2014-12-23 06:17:23 -0700

    Initial Commit
    Added all files used by my 3D Java Game Engine. Will clean up the code
    and comment important sections later on.

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,64 @@
+package engineTester;
+
+import models.RawModel;
+import models.TexturedModel;
+
+import org.lwjgl.opengl.Display;
+
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
+
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+		
+		DisplayManager.createDisplay();
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
+		
+		// OpenGL expects vertices to be defined counter-clockwise by default
+		float[] vertices = {
+				-0.5f, 0.5f, 0,		// V0
+				-0.5f, -0.5f, 0,	// V1
+				0.5f, -0.5f, 0,		// V2
+				0.5f, 0.5f, 0		// V3
+		};
+		
+		int[] indices = {
+				0, 1, 3,	// Top-left triangle (V0,V1,V3)
+				3, 1, 2		// Bottom-right triangle (V3,V1,V2)
+		};
+		
+		float[] textureCoords = {
+				0, 0,	// V0
+				0, 1,	// V1
+				1, 1,	// V2
+				1, 0	// V3
+		};
+		
+		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
+		
+		while(!Display.isCloseRequested()) {
+			// Game Logic
+			renderer.prepare();
+			// Rendering
+			shader.start();
+			renderer.render(texturedModel);
+			shader.stop();
+			DisplayManager.updateDisplay();
+			
+		}
+		
+		shader.cleanUp();
+		loader.cleanUp();
+		DisplayManager.closeDisplay();
+
+	}
+
+}
