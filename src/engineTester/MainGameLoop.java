d65319d5b42d398c054dcd2f1af1feee3612d6db
KJ <kjbrackish@gmail.com>
2014-12-24 00:16:54 -0700

    Implemented 3D OBJ object loader & matrixes Adjusted code to automatically detect the x,y,z points of each model, texture, and shader point in OBJ files from Blender.

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -21,6 +21,7 @@ import org.lwjgl.util.vector.Vector3f;
 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -37,49 +38,18 @@ public class MainGameLoop {
 		Renderer renderer = new Renderer(shader);
 		
 		// OpenGL expects vertices to be defined counter-clockwise by default
-		float[] vertices = {			
-				-0.5f,0.5f,-0.5f,-0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,0.5f,0.5f,-0.5f,		
-				
-				-0.5f,0.5f,0.5f,-0.5f,-0.5f,0.5f,	
-				0.5f,-0.5f,0.5f,0.5f,0.5f,0.5f,
-				
-				0.5f,0.5f,-0.5f,0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,0.5f,0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,-0.5f,-0.5f,-0.5f,-0.5f,	
-				-0.5f,-0.5f,0.5f,-0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,0.5f,-0.5f,0.5f,-0.5f,
-				0.5f,0.5f,-0.5f,0.5f,0.5f,0.5f,
-				
-				-0.5f,-0.5f,0.5f,-0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,0.5f,-0.5f,0.5f
-				
-		};
-		float[] textureCoords = {
-				
-				0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,			
-				0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,
-				0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0
-
-		};
-		int[] indices = {
-				0,1,3,3,1,2,4,5,7,7,5,6,
-				8,9,11,11,9,10,12,13,15,15,13,14,	
-				16,17,19,19,17,18,20,21,23,23,21,22
-
-		};
 		
-		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("image")));
 		
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
+		RawModel model = OBJLoader.loadObjModel("stall", loader);
+		
+		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
+		
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50),0,0,0,1);
 		
 		Camera camera = new Camera();
 		
 		while(!Display.isCloseRequested()) {
-			entity.increaseRotation(1,1,0);
+			entity.increaseRotation(0,1,0);
 			camera.move();
 			// Game Logic
 			renderer.prepare();
